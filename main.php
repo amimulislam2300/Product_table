237da932406ebb84482dddde2f06002526e363a9
Full Name <email@address.com>
2024-12-14 13:08:05 +0600

    solve

--- main.php
+++ main.php
@@ -1,10 +1,18 @@
 <?php
-$conn = new mysqli("localhost", "root", password: "", "product_store");
+$conn =mysqli_connect("localhost", "root", "", "company");
+
 
 if(isset($_POST['btn'])){
     $name = $_POST['name'];
     $product = $_POST['product'];
-    $conn->query("call brand ('$name', '$product')");
+    $conn->query("call brand_n('$name', '$product')");
+}
+if(isset($_POST['btnAdd'])){
+    $name = $_POST['pname'];
+    $price = $_POST['price'];
+    $id = $_POST['brand_id'];
+
+    $conn->query("call product_add('$name','$price','$id')");
 }
 ?>
 
@@ -18,7 +26,7 @@ if(isset($_POST['btn'])){
 </head>
 <body>
     <h2>Brand Table</h2>
-    <form action="#" method="post">
+    <form action="" method="post">
         <label for="">Name</label><br>
          <input type="text" name="name" required><br>
          <label for="">Product</label><br>
@@ -26,5 +34,29 @@ if(isset($_POST['btn'])){
 
         <button type="submit" name="btn">Submit</button>
     </form>
+<br><br>
+    <section>
+    <h2>add product</h2>
+    <form action="" method="post">
+        <label for="">product Name</label><br>
+         <input type="text" name="pname" required><br>
+         <label for="">Price</label><br>
+         <input type="text" name="price" required><br>
+         <label for="">Manufacture:</label>
+         <select name="brand_id" id="">
+            <?php
+            $brand = $conn->query("select * from brand");
+            while(list($id, $name)= $brand->fetch_row()){
+                echo "<option value = '$id'>$name</option>";
+            }
+
+            
+            
+            ?>
+         </select>
+
+        <button type="submit" name="btnAdd">Submit</button>
+    </form>
+    </section>
 </body>
 </html>
